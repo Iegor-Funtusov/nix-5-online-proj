05c924c9f7826130d0a3708378ff8cb5f0aee5b6
Dima Gridin <dmitriygridin0904@gmail.com>
2021-04-21 23:44:04 +0300

    Added id to the figure to identify each object

--- Hw1/src/Figure.java
+++ Hw1/src/Figure.java
@@ -1,5 +1,16 @@
+import java.util.UUID;
+
 public abstract class Figure{
+    private String id;
+
+    Figure(){
+        this.id = UUID.randomUUID().toString();
+    }
 
     public abstract double calcPerimeter();
     public abstract double calcArea();
+
+    public String getId(){
+        return this.id;
+    }
 }
