97dce9faf8c45f59a21678128277b7483727dc80
sudokun <dramviewer@yahoo.com>
2021-04-21 16:20:28 +0300

    implement user entity

---
+++ src/com/nixcourse/Main.java
@@ -0,0 +1,8 @@
+package com.nixcourse;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User(1, "Denis", "Nesterenko");
+        System.out.println(user);
+    }
+}
