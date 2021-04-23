b8b6a3cf4128d3de1afdbc6dba14df1fd98d3705
k4rnaj1k <karnajikplayer@gmail.com>
2021-04-23 19:20:03 +0300

    Commands.bat now compiles and then launches a jar file

--- cmd-compile/commands.bat
+++ cmd-compile/commands.bat
@@ -1,6 +1,11 @@
+@echo off
 title Compiling Crud
 cd ../
 del /f /s /q build 1>nul
 javac -d build/classes -cp libs/lombok-1.18.20.jar;libs/commons-lang3-3.12.0.jar -sourcepath src src/com/k4rnaj1k/Crud.java src/com/k4rnaj1k/AnSecondary.java
-java -cp build/classes;libs/commons-lang3-3.12.0.jar com.k4rnaj1k.Crud
+cd build/classes
+cls
+@echo on
+jar cfm ../jar/Crud.jar ../../cmd-compile/Manifest.txt com/k4rnaj1k/*.class
+java -jar ../jar/Crud.jar
 pause
