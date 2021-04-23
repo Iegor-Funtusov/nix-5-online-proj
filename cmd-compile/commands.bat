f5f44441b89f8722b4f901f72bf5b8bbb31c19f3
k4rnaj1k <karnajikplayer@gmail.com>
2021-04-23 19:40:49 +0300

    commands.bat now creates a jar folder in build if it doesn't exist

--- cmd-compile/commands.bat
+++ cmd-compile/commands.bat
@@ -5,6 +5,9 @@ del /f /s /q build 1>nul
 javac -d build/classes -cp libs/lombok-1.18.20.jar;libs/commons-lang3-3.12.0.jar -sourcepath src src/com/k4rnaj1k/Crud.java src/com/k4rnaj1k/AnSecondary.java
 cd build/classes
 cls
+cd ../
+if not exist jar mkdir jar
+cd classes
 @echo on
 jar cfm ../jar/Crud.jar ../../cmd-compile/Manifest.txt com/k4rnaj1k/*.class
 java -jar ../jar/Crud.jar
