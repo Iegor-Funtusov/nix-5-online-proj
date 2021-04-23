b789aaae8c1053e75c044a8ec77567b9cbc02a67
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-04-24 00:37:38 +0300

    Complited console build project w/ .jar(s) and working classes

---
+++ compile/import/src/ua/com/Main.java
@@ -0,0 +1,12 @@
+package ua.com;
+
+import ua.com.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User("Petya", "Vasychkin");
+        user.print();
+
+    }
+}
