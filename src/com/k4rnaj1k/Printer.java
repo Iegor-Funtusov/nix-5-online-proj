85975d0b27ce48b205fbb9fcee72976fd523774b
k4rnaj1k <karnajikplayer@gmail.com>
2021-04-24 15:35:57 +0300

    Renamed main class to represent better what it does

--- src/com/k4rnaj1k/Printer.java
+++ src/com/k4rnaj1k/Printer.java
@@ -3,11 +3,11 @@ import org.apache.commons.lang3.*;
 
 import java.util.Locale;
 
-public class Crud {
+public class Printer {
     public static void main(String[] args) {
         System.out.println("Main in crud.");
-        Secondary crud2 = new Secondary();
-        crud2.print();
+        Secondary print2 = new Secondary();
+        print2.print();
         System.out.println(StringUtils.upperCase("lower case string to upper case using string utils"));
         AnSecondary anSecondary = new AnSecondary();
         System.out.println("local string after creating an instance of another secondary class: " + anSecondary.getLocals());
