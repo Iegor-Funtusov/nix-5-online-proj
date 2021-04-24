66e969206b3d1314b17dd66dcf7b0a758e5e11cb
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-04-24 17:57:04 +0300

    Create array reverser

--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
@@ -4,6 +4,7 @@ import main.java.ua.com.nkrasnovoronka.unit2.task1.Task1;
 import main.java.ua.com.nkrasnovoronka.unit2.task2.Task2;
 import main.java.ua.com.nkrasnovoronka.unit2.task3.Task3;
 import main.java.ua.com.nkrasnovoronka.unit2.task4.Task4;
+import main.java.ua.com.nkrasnovoronka.unit2.task5.Task5;
 import main.java.ua.com.nkrasnovoronka.unit2.util.Util;
 
 public class Main {
@@ -12,7 +13,8 @@ public class Main {
 //        Task1.arrayEvenNumbersPrinter(arrayFromUserInput);
 //        Task2.calculateNumberOfPositiveNumber(arrayFromUserInput);
 //        Task3.countNextNumberInArray(arrayFromUserInput);
-        Task4.countArrayNeighbors(arrayFromUserInput);
+//        Task4.countArrayNeighbors(arrayFromUserInput);
+        Task5.reverseArray(arrayFromUserInput);
     }
 
 }
--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task5/Task5.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task5/Task5.java
@@ -1,4 +1,27 @@
 package main.java.ua.com.nkrasnovoronka.unit2.task5;
 
+import java.util.Arrays;
+
+/**
+ * Напишите программу, которая переставляет элементы массива в обратном порядке
+ * без использования дополнительного массива. Программа должна считать массив,
+ * поменять порядок его элементов, затем вывести результат (просто вывести
+ * элементы массива в обратном порядке – недостаточно!). Необходимо вывести
+ * массив, полученный после перестановки элементов.
+ */
 public class Task5 {
+    public static void reverseArray(int [] array){
+        System.out.println("Reversing array");
+        int tmp;
+        for (int i = 0; i < array.length / 2; i++) {
+            tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+
+        System.out.println(Arrays.toString(array));
+
+
+
+    }
 }
