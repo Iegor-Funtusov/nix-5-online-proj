45b3967b126fe371e840045cd05c707cd96ef9fc
NickM-10 <maltsevn10@gmail.com>
2021-04-24 16:25:59 +0300

    add classes

---
+++ DZ_1/package/ua/com/alevel/Hello.java
@@ -0,0 +1,8 @@
+package ua.com.alevel;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ DZ_1/package/ua/com/alevel/Usr.java
@@ -0,0 +1,6 @@
+package ua.com.alevel;
+
+public class Usr {
+    private String id;
+    private String name;
+}
