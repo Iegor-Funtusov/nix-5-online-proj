88be1047a10ae12dd82d7d1d8564e30eb3276919
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-04-24 17:37:07 +0300

    Create array positive numbers counter

--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
@@ -1,11 +1,13 @@
 package main.java.ua.com.nkrasnovoronka.unit2;
 
 import main.java.ua.com.nkrasnovoronka.unit2.task1.Task1;
+import main.java.ua.com.nkrasnovoronka.unit2.task2.Task2;
 import main.java.ua.com.nkrasnovoronka.unit2.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         int[] arrayFromUserInput = Util.createArrayFromUserInput();
         Task1.arrayEvenNumbersPrinter(arrayFromUserInput);
+        Task2.calculateNumberOfPositiveNumber(arrayFromUserInput);
     }
 }
--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task2/Task2.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task2/Task2.java
@@ -1,4 +1,17 @@
 package main.java.ua.com.nkrasnovoronka.unit2.task2;
 
+import java.util.Arrays;
+
+/**
+ * Дан массив, состоящий из целых чисел. Напишите программу, которая
+ * подсчитывает количество положительных чисел среди элементов массива.
+ * Необходимо единственное число - количество положительных элементов в
+ * массиве.
+ */
 public class Task2 {
+    public static void calculateNumberOfPositiveNumber(int[] array){
+        System.out.println("Calculating positive numbers in array");
+        long count = Arrays.stream(array).filter(value -> value > 0).count();
+        System.out.println(count);
+    }
 }
