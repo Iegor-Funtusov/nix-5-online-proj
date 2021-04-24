dcc13d6db52a7622d102bc1e535f5eb765ce6dab
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-04-24 17:33:03 +0300

    Create array event numbers printer

--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
@@ -1,11 +1,11 @@
 package main.java.ua.com.nkrasnovoronka.unit2;
 
+import main.java.ua.com.nkrasnovoronka.unit2.task1.Task1;
 import main.java.ua.com.nkrasnovoronka.unit2.util.Util;
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] arrayFromUserInput = Util.createArrayFromUserInput();
+        Task1.arrayEvenNumbersPrinter(arrayFromUserInput);
     }
 }
--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task1/Task1.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task1/Task1.java
@@ -1,4 +1,21 @@
 package main.java.ua.com.nkrasnovoronka.unit2.task1;
 
+/**
+ * Дан массив, состоящий из целых чисел. Напишите программу, которая выводит те
+ * элементы массива, которые являются чётными числами.
+ * Необходимо вывести все четные элементы массива (то есть те элементы, которые
+ * являются четными числами).
+ */
 public class Task1 {
+    public static void arrayEvenNumbersPrinter(int[] array) {
+        System.out.println("Printing all even numbers from array");
+        StringBuilder sb = new StringBuilder();
+        for (int i : array) {
+            if (i % 2 == 0) {
+                sb.append(i).append(" ");
+            }
+        }
+        System.out.println(sb);
+
+    }
 }
