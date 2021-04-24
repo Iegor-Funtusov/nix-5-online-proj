8e8848be98cc164aa29a3819f738c5f514c34bd7
Sheblyk <kokosigel@gmail.com>
2021-04-24 18:30:01 +0300

    added ability to compile in maven

---
+++ maven_compile/src/main/java/entity/User.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class User {
+    private String name;
+    private String surname;
+    public static void print(){
+        System.out.println("Hi, I`m a user!");
+    }
+}
+
