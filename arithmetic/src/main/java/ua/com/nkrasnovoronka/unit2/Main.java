403d6c5545bea494096b7f6e1a5306dbc5f4b67d
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-04-24 20:30:25 +0300

    Create array swapping neighbors

--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/Main.java
@@ -5,16 +5,18 @@ import main.java.ua.com.nkrasnovoronka.unit2.task2.Task2;
 import main.java.ua.com.nkrasnovoronka.unit2.task3.Task3;
 import main.java.ua.com.nkrasnovoronka.unit2.task4.Task4;
 import main.java.ua.com.nkrasnovoronka.unit2.task5.Task5;
+import main.java.ua.com.nkrasnovoronka.unit2.task6.Task6;
 import main.java.ua.com.nkrasnovoronka.unit2.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         int[] arrayFromUserInput = Util.createArrayFromUserInput();
-//        Task1.arrayEvenNumbersPrinter(arrayFromUserInput);
+        Task1.arrayEvenNumbersPrinter(arrayFromUserInput);
 //        Task2.calculateNumberOfPositiveNumber(arrayFromUserInput);
 //        Task3.countNextNumberInArray(arrayFromUserInput);
 //        Task4.countArrayNeighbors(arrayFromUserInput);
-        Task5.reverseArray(arrayFromUserInput);
+//        Task5.reverseArray(arrayFromUserInput);
+//        Task6.swapNeighbors(arrayFromUserInput);
     }
 
 }
--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task6/Task6.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task6/Task6.java
@@ -1,4 +1,23 @@
 package main.java.ua.com.nkrasnovoronka.unit2.task6;
 
+import java.util.Arrays;
+
+/**
+ * Напишите программу, которая переставляет соседние элементы массива (1-й
+ * элемент поменять с 2-м, 3-й с 4-м и т.д. Если элементов нечетное число, то
+ * последний элемент остается на своем месте). Необходимо вывести массив,
+ * полученный после перестановки элементов.
+ */
 public class Task6 {
+    public static void swapNeighbors(int[] array) {
+        System.out.println("Swapping neighbors");
+        int tmp;
+        for (int i = 1; i < array.length; i = i + 2) {
+            tmp = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = tmp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
 }
