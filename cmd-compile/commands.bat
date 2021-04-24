cb0bc74855724857e6d4e5d92e3e80458b1cb447
k4rnaj1k <karnajikplayer@gmail.com>
2021-04-24 15:35:05 +0300

    Edited commands.bat

--- cmd-compile/commands.bat
+++ cmd-compile/commands.bat
@@ -1,14 +1,14 @@
 @echo off
-title Compiling Crud
+title Compiling Printer
 cd ../
-del /f /s /q build 1>nul
-javac -d build/classes -cp libs/lombok-1.18.20.jar;libs/commons-lang3-3.12.0.jar -sourcepath src src/com/k4rnaj1k/Crud.java src/com/k4rnaj1k/AnSecondary.java
+if exist "build" rmdir /s "build"
+javac -d build/classes -cp libs/lombok-1.18.20.jar;libs/commons-lang3-3.12.0.jar -sourcepath src src/com/k4rnaj1k/Printer.java src/com/k4rnaj1k/AnSecondary.java
 cd build/classes
 cls
 cd ../
 if not exist jar mkdir jar
 cd classes
+jar cfm ../jar/Printer.jar ../../cmd-compile/Manifest.txt com/k4rnaj1k/*.class
 @echo on
-jar cfm ../jar/Crud.jar ../../cmd-compile/Manifest.txt com/k4rnaj1k/*.class
-java -jar ../jar/Crud.jar
+java -jar ../jar/Printer.jar
 pause
