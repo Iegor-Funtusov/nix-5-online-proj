d6fe0d7497f5509a6ebc1ce3fcb9c3cdb6a75f7f
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-04-24 17:43:13 +0300

    Create next array value counter

--- arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task3/Task3.java
+++ arithmetic/src/main/java/ua/com/nkrasnovoronka/unit2/task3/Task3.java
@@ -1,4 +1,21 @@
 package main.java.ua.com.nkrasnovoronka.unit2.task3;
 
+/**
+ * Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает
+ * количество элементов массива, больших предыдущего (элемента с предыдущим
+ * номером). Необходимо вывести единственное число - количество элементов
+ * массива, больших предыдущего.
+ */
 public class Task3 {
+    public static void countNextNumberInArray(int[] array) {
+        System.out.println("Counting next value in array");
+        int count = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[i - 1]){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
 }
