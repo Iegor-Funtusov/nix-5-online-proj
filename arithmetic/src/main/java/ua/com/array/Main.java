0de6efdf6cb628c22e680a186c38a761f0c85288
Serhii-Semuso <serhiysemuso@gmail.com>
2021-04-26 02:09:22 +0300

    Added calls for all methods from the ArrayArithmetic in the class Main .

--- arithmetic/src/main/java/ua/com/array/Main.java
+++ arithmetic/src/main/java/ua/com/array/Main.java
@@ -6,14 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayArithmetic array = new ArrayArithmetic(createRandomArray());
+        System.out.println("Array: " + array);
+
+        System.out.print("Even numbers:");
+        array.printEvenNumbers();
+
+        System.out.println("\nThe number of positive numbers: "
+                + array.numberOfPositiveElements());
+
+        System.out.println("The number of elements that are bigger than previous one: "
+                + array.numberOfBiggerThanPrevious());
+
+        System.out.println("The number of elements that are bigger than their neighbors: "
+                + array.numberOfBiggerThanNeighbors());
+
+        array.reverseArray();
+        System.out.println("Reversed array: " + array);
+
+        array.reshufflePair();
+        System.out.println("Array after pair reshuffle: " + array);
     }
 
     public static int[] createRandomArray(){
         Random random = new Random();
-        int n = random.nextInt(100) + 1;
+        int n = random.nextInt(5) + 5;
         int[] array = new int[n];
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100);
+            array[i] = random.nextInt(100) - 50;
         }
         return array;
     }
