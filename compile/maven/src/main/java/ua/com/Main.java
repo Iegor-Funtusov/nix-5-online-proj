3665ea56a32f4bc79167fa71e186cdd802a2a1ee
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-04-25 13:00:26 +0300

    Maven building version upload

---
+++ compile/maven/src/main/java/ua/com/Main.java
@@ -0,0 +1,10 @@
+package ua.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User("Petya", "Vasychkin");
+        user.print();
+
+    }
+}
---
+++ compile/maven/src/main/java/ua/com/User.java
@@ -0,0 +1,49 @@
+package ua.com;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.math3.random.RandomDataGenerator;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String surname;
+
+    public User(){
+    }
+
+    public User(String name, String surname) {
+        this.id = new RandomDataGenerator().nextInt(1, 100);
+        this.name = StringUtils.upperCase(name);
+        this.surname = StringUtils.upperCase(surname);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void print(){
+        System.out.println("First Name: " + this.name + "\nSurname: " + this.surname);
+    }
+
+}
