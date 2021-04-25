5b012e2fde7942697dfe113d2934db8ad27a8cd0
Anastasiia <anastasiiadubina@gmail.com>
2021-04-26 00:22:08 +0300

    added classes for working with Patient Information

---
+++ console_compile/src/ua/example1/PatientCommunication.java
@@ -0,0 +1,16 @@
+package ua.example1;
+
+public class PatientCommunication {
+
+    public static void greeting() {
+        System.out.println("Hello! We are glad to see you here ;) ");
+    }
+
+    public static void firstMeet() {
+        System.out.println("For your success registration you need to answer for a few question.");
+    }
+
+    public static void congratulations() {
+        System.out.println("Thank you for registration!");
+    }
+}
