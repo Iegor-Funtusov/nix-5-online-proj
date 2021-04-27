9b1984dcda3510efb356fa8b4e6d4b19cb1b79fa
k4rnaj1k <karnajikplayer@gmail.com>
2021-04-27 20:02:08 +0300

    Minor changes to run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-mvn javafx:run
+@echo off
+set M2_HOME=%~dp0apache-maven-3.8.1
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx512m -Dfile.encoding=UTF-8
+set PATH=%M2%;%PATH%
+cls
+@echo on
+if exist "target" rmdir /s "target"
+call mvn javafx:run
+pause
