1c2d3a974f67cfb78a171a3a1d51a20b792f74a9
Andrey <andrey.karintsev@gmail.com>
2021-04-27 13:17:01 +0300

    (unit_1_compile): base implementation of todo-list, etc. add new,show

---
+++ threadedCode/unit_1_compile/console_build/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CommandLine commandLine = new CommandLine();
+        commandLine.readConsole();
+    }
+}
---
+++ threadedCode/unit_1_compile/console_build/TodoList.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private List<Task> tasksStorage = new ArrayList<>();
+
+    public void addTask(BufferedReader reader) throws IOException {
+        Task task = new Task();
+
+        System.out.println("enter title");
+        String title = reader.readLine();
+        task.setTitle(title);
+
+        System.out.println("enter descriptions");
+        String description = reader.readLine();
+        task.setDescription(description);
+
+        addTask(task);
+    }
+
+    public void addTask(Task task) {
+        tasksStorage.add(task);
+    }
+
+    public void showTask() {
+        tasksStorage.stream()
+                .forEach(item -> System.out.println(item.getTitle() + ": " +
+                        item.getDescription() + "\n" +
+                        "=================="));
+        System.exit(0);
+    }
+}
