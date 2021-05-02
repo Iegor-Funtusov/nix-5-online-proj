2ad4cea690432d4346280756e63c711d9b301c47
Sheblyk <kokosigel@gmail.com>
2021-05-02 13:36:06 +0300

    added input validation

--- chess/src/main/java/ChessRunService.java
+++ chess/src/main/java/ChessRunService.java
@@ -9,26 +9,11 @@ public class ChessRunService {
         Board board = new Board();
         boolean cycle = true;
         while (cycle) {
-            System.out.println("Please, choose the colour of your figures");
-            System.out.println("1 -> Red");
-            System.out.println("2 -> White");
             Scanner s = new Scanner(System.in);
-            int colour = s.nextInt();
-            System.out.println("Please, choose the figure");
-            System.out.println("1 -> King");
-            System.out.println("2 -> Queen");
-            System.out.println("3 -> Knight");
-            System.out.println("4 -> Bishop");
-            System.out.println("5 -> Rook");
-            System.out.println("6 -> Pawn");
-            int chosenfigure = s.nextInt();
-            System.out.println("Please, enter the x: 1-8");
-            int x = s.nextInt();
-            System.out.println("Please, enter the y: A-H");
-            char y = s.next().charAt(0);
-            if (colour == 1) {
-                x = 9 - x;
-            }
+            int colour = Checker.setColor();
+            int chosenfigure = Checker.setFigire();
+            int x = Checker.setX(colour);
+            char y = Checker.setY();
             if (!(board.checkEmptyCell(x - 1, CreateFigure.defineY(y)))) {
                 System.out.println("This cell is not empty!");
             } else {
@@ -56,14 +41,8 @@ public class ChessRunService {
 
     public static void movement(int colour, Board board) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Please, enter the x: 1-8");
-        int x = s.nextInt();
-        if (colour == 1) {
-            x = 9 - x;
-        }
-        System.out.println("Please, enter the y: A-H");
-        char y = s.next().charAt(0);
-        System.out.println(x + "  " + y);
+        int x = Checker.setX(colour);
+        char y = Checker.setY();
         Figure f = Board.allfigures.get(Board.allfigures.size() - 1);
         while (true) {
             if (f.run(x - 1, CreateFigure.defineY(y))) {
@@ -76,15 +55,11 @@ public class ChessRunService {
                 break;
             } else {
                 System.out.println("You can not make this move!");
-                System.out.println("Please, enter the x: 1-8");
-                x = s.nextInt();
-                if (colour == 1) {
-                    x = 9 - x;
-                }
-                System.out.println("Please, enter the y: A-H");
-                y = s.next().charAt(0);
+                x = Checker.setX(colour);
+                y = Checker.setY();
                 f = Board.allfigures.get(Board.allfigures.size() - 1);
             }
         }
     }
+
 }
