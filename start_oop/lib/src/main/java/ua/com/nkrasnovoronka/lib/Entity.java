5864ed6ba9987a10337c0f8cf9ce0be81a1c50c7
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-05-06 14:14:26 +0300

    create abstract entity class and crud interface

---
+++ start_oop/lib/src/main/java/ua/com/nkrasnovoronka/lib/Entity.java
@@ -0,0 +1,20 @@
+package ua.com.nkrasnovoronka.lib;
+
+public abstract class Entity {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
