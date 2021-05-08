9fe2dffc316815b7a4cb22c1c18e23f1108c8dba
Anastasiia <anastasiiadubina@gmail.com>
2021-05-09 00:43:44 +0300

    added main class for launch application

---
+++ chess/src/main/java/ua/com/alevel/ChessApplication.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+import java.io.IOException;
+
+import static ua.com.alevel.util.Util.*;
+
+public class ChessApplication {
+
+    public static void main(String[] args) throws IOException {
+        start();
+    }
+}
