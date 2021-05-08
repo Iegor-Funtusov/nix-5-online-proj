17a1743b15ed9d257bcd147c81ee0612f5b49de9
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-08 20:57:47 +0300

    added Book data class

---
+++ start_oop/app/src/main/java/com/nixsolutions/courses/app/Book.java
@@ -0,0 +1,34 @@
+package com.nixsolutions.courses.app;
+
+import com.nixsolutions.courses.lib.BaseEntity;
+
+public class Book extends BaseEntity {
+
+    private int year;
+    private String title;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + super.getId() +
+                ", year=" + year +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
