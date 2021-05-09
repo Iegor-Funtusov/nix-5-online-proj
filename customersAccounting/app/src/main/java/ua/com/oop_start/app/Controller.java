f0bd90b68d681fcb5a39da3a538d64de5dc9e12b
Sheblyk <kokosigel@gmail.com>
2021-05-09 16:31:27 +0300

    created console access

---
+++ customersAccounting/app/src/main/java/ua/com/oop_start/app/Controller.java
@@ -0,0 +1,89 @@
+package ua.com.oop_start.app;
+
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Controller {
+    public static void getFunctions(){
+        Scanner scanner = new Scanner(System.in);
+        CustomerService customerService = new CustomerService();
+        Collection<Customer> set;
+        boolean cycle = true;
+
+        while(cycle){
+         System.out.println("What would you like to get?");
+         System.out.println("1 -> add customer");
+         System.out.println("2 -> update customer");
+         System.out.println("3 -> delete customer");
+         System.out.println("4 -> read customer by name");
+         System.out.println("5 -> exit");
+         set = customerService.readAll();
+         set.forEach(System.out::println);
+         int chioce = scanner.nextInt();
+
+         switch (chioce){
+             case 1: {
+                 Customer customer = new Customer();
+                 System.out.println("Enter customer name: ");
+                 String name = scanner.next();
+                 customer.setName(name);
+                 System.out.println("Enter the sum of purchase: ");
+                 int sum = scanner.nextInt();
+                 customer.setSumofpurchase(sum);
+                 customerService.create(customer);
+                 break;
+             }
+             case 2: {
+                 System.out.println("Enter customer name: ");
+                 String name = scanner.next();
+                 System.out.println("Enter the sum of NEXT purchase: ");
+                 int sum = scanner.nextInt();
+                 for(Customer c : set){
+                     if(c.getName().equals(name)){
+                         c.setSumofpurchase(c.getSumofpurchase() + sum);
+                         customerService.update(c);
+                         break;
+                     }
+                 }
+                 System.out.println("Customer with " + name + " doesn`t exist");
+                 break;
+             }
+             case 3: {
+                 System.out.println("Enter customer name: ");
+                 String name = scanner.next();
+                 for(Customer c : set){
+                     if(c.getName().equals(name)){
+                         customerService.delete(c.getId());
+                         break;
+                     }
+                     if(set.stream().noneMatch(e -> e.getName().equals(name))){
+                         System.out.println("Customer with " + name  + " doesn`t exist");
+                     }
+                 }
+                 break;
+             }
+             case 4: {
+                 System.out.println("Enter customer name: ");
+                 String name = scanner.next();
+                 for (Customer c : set) {
+                     if (c.getName().equals(name)) {
+                         System.out.println(customerService.read(c.getId()));
+                         break;
+                     }
+                 }
+                 System.out.println("Customer with " + name + " doesn`t exist");
+                 break;
+             }
+             case 5: {
+                 cycle = false;
+                 break;
+             }
+             default: {
+                 System.out.println("Please, enter number from 1 to 5 : " );
+                 break;
+             }
+         }
+         }
+     }
+    }
+
--- customersAccounting/app/src/main/java/ua/com/oop_start/app/EnterPoint.java
+++ customersAccounting/app/src/main/java/ua/com/oop_start/app/EnterPoint.java
@@ -1,50 +1,7 @@
 package ua.com.oop_start.app;
-import java.util.Collection;
 
 public class EnterPoint {
     public static void main(String[] args) {
-        CustomerService customerService = new CustomerService();
-        Customer customer = new Customer();
-        customer.setName("Вася");
-        customer.setSumofpurchase(349);
-        customerService.create(customer);
-
-        Customer customer2 = new Customer();
-        customer2.setName("Аня");
-        customer2.setSumofpurchase(2789);
-        customerService.create(customer2);
-
-        Collection<Customer> set = customerService.readAll();
-        set.forEach(System.out::println);
-
-        System.out.println(" ");
-        for(Customer c : set){
-            if(c.getName().equals("Вася")){
-                c.setSumofpurchase(c.getSumofpurchase() + 1000);
-                customerService.update(c);
-            }
-        }
-        set.forEach(System.out::println);
-
-        System.out.println(" ");
-        String fordel = null;
-        for(Customer c : set){
-            if(c.getName().equals("Вася")){
-                fordel = c.getId();
-            }
-            if (fordel!= null){
-                customerService.delete(fordel);
-                break;
-            }
-        }
-        set.forEach(System.out::println);
-
-        for(Customer c : set){
-            if(c.getName().equals("Аня")) {
-                String f = c.getId();
-                System.out.println(customerService.read(f));
-            }
-        }
+    Controller.getFunctions();
     }
-
 }
