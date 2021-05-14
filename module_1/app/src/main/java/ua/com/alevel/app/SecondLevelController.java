903f3291f304debf45851cd4656512ddd45a2138
Illia Fedotov <illia.fedotov@nure.ua>
2021-05-15 00:05:18 +0300

    added implement to StringValidator

--- module_1/app/src/main/java/ua/com/alevel/app/SecondLevelController.java
+++ module_1/app/src/main/java/ua/com/alevel/app/SecondLevelController.java
@@ -1,10 +1,20 @@
 package ua.com.alevel.app;
 
+import ua.com.alevel.lib.StringValidator;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SecondLevelController {
 
     public static void stringValidator() throws IOException {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Input string you want to validate: ");
+        String input = reader.readLine();
+
+        System.out.println("Is this string valid: " + StringValidator.isValid(input));
     }
 }
