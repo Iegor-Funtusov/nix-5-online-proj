ec1510bdbd46ed3549f782fe92c94260b5f46e3e
Serhii-Semuso <serhiysemuso@gmail.com>
2021-05-15 17:46:18 +0300

    created class CrudServiceFactory for getting instances of a class that implements CrudService.

---
+++ start_oop/lib/src/main/java/ua/com/alevel/lib/CrudServiceFactory.java
@@ -0,0 +1,51 @@
+package ua.com.alevel.lib;
+
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CrudServiceFactory {
+
+    private static CrudServiceFactory instance;
+    private static final String ROOT_PACKAGE = "ua.com.alevel.lib";
+
+    private final Set<Class<? extends CrudService>> crudServicesSet;
+
+    private CrudServiceFactory(){
+        Reflections reflections = new Reflections(ROOT_PACKAGE);
+        crudServicesSet = reflections.getSubTypesOf(CrudService.class);
+    }
+
+    public static CrudServiceFactory getInstance(){
+        if (instance == null) {
+            instance = new CrudServiceFactory();
+        }
+        return instance;
+    }
+
+    public static CrudService getCrudService(){
+        Set<Class<? extends CrudService>> classes = new HashSet<>();
+        for (Class<? extends CrudService> crudServiceImpl : getInstance().crudServicesSet) {
+            if(!crudServiceImpl.isAnnotationPresent(Deprecated.class))
+                classes.add(crudServiceImpl);
+        }
+        if(classes.size() != 1)
+            throw new RuntimeException("Invalid number of implementations(must be one)!");
+        for (Class<? extends CrudService> crudServiceImpl : classes) {
+            if(!crudServiceImpl.isAnnotationPresent(Deprecated.class)) {
+                try {
+                    return crudServiceImpl.getDeclaredConstructor().newInstance();
+                } catch (InstantiationException
+                        | IllegalAccessException
+                        | InvocationTargetException
+                        | NoSuchMethodException e) {
+                    throw new RuntimeException("Can't create implementation!");
+                }
+            }
+        }
+        throw new RuntimeException("Can't create implementation!!");
+    }
+
+}
