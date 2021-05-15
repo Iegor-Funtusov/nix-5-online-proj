0735322c2069adecf634fd30cb1c0a86221886c6
Evgeniya Lyahovec <jacky@ukr.net>
2021-05-15 23:15:50 +0300

    reformated code in Game.java

--- module_1/src/main/java/level3/Game.java
+++ module_1/src/main/java/level3/Game.java
@@ -12,11 +12,11 @@ public class Game {
     private int generation;
     public static boolean[][] boardC;
 
-	public Game(int m, int n) {
-			this.m = m;
-            this.n = n;
-			this.generation = 0;
-		}
+    public Game(int m, int n) {
+        this.m = m;
+        this.n = n;
+        this.generation = 0;
+    }
 
     public static void Main() {
         Scanner inputUser = new Scanner(System.in);
@@ -48,55 +48,55 @@ public class Game {
         boardC = temp;
     }
 
-		//печать доски
+    //печать доски
     public void printBoard() {
-			//System.out.println();
-			System.out.println("Поколение : " + generation);
-			//System.out.println();
-			for (int i = 0; i < m; i++) {
-				for (int j = 0; j <n; j++) {
-					if (boardC[i][j])
-						System.out.print("#");
-					else
-						System.out.print(".");
-					System.out.print(" ");
-				}
-				System.out.println();
-			}
-			System.out.println();
-		//	System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~");
-		}
+        //System.out.println();
+        System.out.println("Поколение : " + generation);
+        //System.out.println();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (boardC[i][j])
+                    System.out.print("#");
+                else
+                    System.out.print(".");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        //	System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~");
+    }
 
-		// создание новой доски
-        public void generateNewBoard() {
-			boolean temp[][] = new boolean[m][n];
-			for (int i = 0; i < m; i++) {
-				for (int j = 0; j < n; j++)
-					temp[i][j] = check(i,j);
-			}
-			boardC = temp;
-			generation++;
-		}
+    // создание новой доски
+    public void generateNewBoard() {
+        boolean temp[][] = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                temp[i][j] = check(i, j);
+        }
+        boardC = temp;
+        generation++;
+    }
 
-		//проверяет, будет жить или умрёт ячейка в следующем поколении
-		public boolean check(int row, int col) {
-			boolean curr = boardC[row][col];
-			int liveNeighbours = 0;
-			for (int i = (-1); i <= 1; i++) {
-				int r = row + i;
-				if (r < 0 || r >= m)
-					continue;
-				for (int j = (-1); j <= 1; j++) {
-					int c = col + j;
-					if (c < 0 || c >= n)
-						continue;
-					if (boardC[r][c])
-						liveNeighbours++;
-				}
-			}
-			if (curr) liveNeighbours--;
-			if (liveNeighbours == 2 && curr)
-				return true;
-			else return liveNeighbours == 3;
-		}
+    //проверяет, будет жить или умрёт ячейка в следующем поколении
+    public boolean check(int row, int col) {
+        boolean curr = boardC[row][col];
+        int liveNeighbours = 0;
+        for (int i = (-1); i <= 1; i++) {
+            int r = row + i;
+            if (r < 0 || r >= m)
+                continue;
+            for (int j = (-1); j <= 1; j++) {
+                int c = col + j;
+                if (c < 0 || c >= n)
+                    continue;
+                if (boardC[r][c])
+                    liveNeighbours++;
+            }
+        }
+        if (curr) liveNeighbours--;
+        if (liveNeighbours == 2 && curr)
+            return true;
+        else return liveNeighbours == 3;
+    }
 }
