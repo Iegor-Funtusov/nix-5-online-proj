f7cd3a6ecb486a18a7eb1ab3b03ba3fe103ec5ed
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-15 13:27:32 +0300

    refactoring

--- unit_5/app/src/main/java/com/nixsolutions/courses/app/AppMain.java
+++ unit_5/app/src/main/java/com/nixsolutions/courses/app/AppMain.java
@@ -1,9 +1,12 @@
 package com.nixsolutions.courses.app;
 
+import java.io.BufferedReader;
+
 public class AppMain {
 
     public static void main(String[] args) {
         System.out.println("AppMain.main");
+        ReverseController.run();
     }
 
 }
