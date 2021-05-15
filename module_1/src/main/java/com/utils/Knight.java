8fec180e1aa13f60a0e3d668789b50cfc6c4727a
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-15 23:55:18 +0300

    Knight class for KnightMove task added

---
+++ module_1/src/main/java/com/utils/Knight.java
@@ -0,0 +1,29 @@
+package com.utils;
+
+public class Knight {
+
+    private int x;
+    private int y;
+    public final String symbol = "♘";
+
+    public Knight(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
