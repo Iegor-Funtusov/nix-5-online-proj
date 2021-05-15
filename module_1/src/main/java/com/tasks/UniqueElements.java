20b7ac03ffb0da2680adf3c035d0e9a79c0c3543
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-15 23:54:39 +0300

    level 1 task 1 added

--- module_1/src/main/java/com/tasks/UniqueElements.java
+++ module_1/src/main/java/com/tasks/UniqueElements.java
@@ -1,4 +1,17 @@
 package com.tasks;
 
+import java.util.ArrayList;
+
 public class UniqueElements {
+    public static int task1(int[] arr) {
+        ArrayList<Integer> sum = new ArrayList<>();
+        int end_sum = 0;
+        for (int j : arr) {
+            sum.set(j, 1);
+        }
+        for (Integer integer : sum) {
+            end_sum += integer;
+        }
+        return end_sum;
+    }
 }
