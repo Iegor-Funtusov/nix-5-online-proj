3332785c4fe6680ac4e4828ab0aa1b3ea8d8855c
Illia Fedotov <illia.fedotov@nure.ua>
2021-05-15 03:16:58 +0300

    added GameLifeController

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# nix-5-online-proj
+# module_1
+>run AppMain.java
+- Choose the level (1-3) or stop the program (0)
+####Level 1
+1. Count amount of unique elements in array (enter array or randomize it)
+2. Validate the knight turn on infinitive board (enter starter kords and destination kords)
+3. Find the triangle area (enter kords of triangle points A,B,C or randomize them)
+0. Back to main menu
+####Level 2
+Validate the string by brackets and there sequence (enter the string)
+####Level 3
+"Game of life" 
+1. enter size of the board
+2. enter elments (0-dead, 1-alive) or randomize them
+- enter 1 for changing into next state
+- enter 2 for printing the board
+- enter 1 for ending the game
--- module_1/app/src/main/java/ua/com/alevel/app/controllers/GameLifeController.java
+++ module_1/app/src/main/java/ua/com/alevel/app/controllers/GameLifeController.java
@@ -2,26 +2,84 @@ package ua.com.alevel.app.controllers;
 
 import ua.com.alevel.lib.LifeGame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class GameLifeController {
 
     public static void gameLife() {
 
-        int[][] board = {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0, 0},
-                {0, 1, 0, 1, 0, 0, 0, 0, 0},
-                {0, 0, 1, 1, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0}
-        };
-        LifeGame.printBoard(board.length, board[0].length, board);
-        System.out.println("====================================================");
-        LifeGame.gameOfLife(board);
+        Scanner s = new Scanner(System.in);
+        System.out.println("You need to input a board, choose the way of input:\n" +
+                "1 -> Do it by yourself\n" +
+                "2 -> Random input");
+        int choice = s.nextInt();
 
+        if (choice == 1) {
+            System.out.print("Input board length: ");
+            int length = s.nextInt();
+            System.out.print("Input board height: ");
+            int height = s.nextInt();
+            int[][] board = new int[length][height];
+            System.out.println("Input elements (0-dead or 1-alive): ");
+            for (int i = 0; i < length; i++) {
+                for (int j =0; j < height; j++) {
+                    board[i][j] = s.nextInt();
+                }
+            }
+            LifeGame.printBoard(board.length, board[0].length, board);
+            changeState(board);
+        }
+        else if (choice == 2) {
+            System.out.print("Input board length: ");
+            int length = s.nextInt();
+            System.out.print("Input board height: ");
+            int height = s.nextInt();
+            int[][] board = new int[length][height];
+            for (int i = 0; i < length; i++) {
+                for (int j =0; j < height; j++) {
+                    board[i][j] = (int) Math.round(Math.random());
+                }
+            }
+            LifeGame.printBoard(board.length, board[0].length, board);
+            changeState(board);
+        }
+    }
 
+    public static void changeState(int[][] board) {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        try {
+            System.out.println("Choose the action:\n" +
+                    "1 -> Next state\n" +
+                    "2 -> Print board\n" +
+                    "0 -> End the game");
+            while ((input = reader.readLine())!= null) {
+                switch (input) {
+                    case "1" : {
+                        LifeGame.gameOfLife(board);
+                    } break;
+                    case "2" : {
+                        LifeGame.printBoard(board.length, board[0].length, board);
+                    } break;
+                    case "0": {
+                        return;
+                    }
+                    default: {
+                        System.out.println("Wrong input");
+                    } break;
+                }
+                System.out.println("Choose the action:\n" +
+                        "1 -> Next state\n" +
+                        "2 -> Print board\n" +
+                        "0 -> End the game");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
