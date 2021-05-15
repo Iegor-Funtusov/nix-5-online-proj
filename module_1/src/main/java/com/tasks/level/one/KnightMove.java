f273f6e126c8caa4b198e869d5438d9f841df789
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-16 01:43:38 +0300

    Knight Move task added

--- module_1/src/main/java/com/tasks/level/one/KnightMove.java
+++ module_1/src/main/java/com/tasks/level/one/KnightMove.java
@@ -1,4 +1,12 @@
 package com.tasks.level.one;
 
+import com.utils.Board;
+
 public class KnightMove {
+
+    public static void Move(int posx, int posy, int movex, int movey) {
+        Board b = new Board(posx, posy);
+        b.PlacePiece(movex, movey);
+        b.Print();
+    }
 }
