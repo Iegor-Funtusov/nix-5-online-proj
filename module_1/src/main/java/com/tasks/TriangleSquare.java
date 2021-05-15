f4d1cdd1eacda2bcc05e47b8c5cfcd81335f4bd2
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-16 01:00:04 +0300

    Triangle Square task solution added

---
+++ module_1/src/main/java/com/tasks/TriangleSquare.java
@@ -0,0 +1,15 @@
+package com.tasks;
+
+public class TriangleSquare {
+
+    public static double getSuare(int a, int b, int c) {
+        double side_a = getSide(a, b);
+        double side_b = getSide(b, c);
+        double side_c = getSide(a, c);
+        double p = (side_a + side_b + side_c) / 2;
+        return Math.sqrt(p * (p - side_a) * (p - side_b) * (p - side_c));
+    }
+    public static double getSide(int a, int b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+}
