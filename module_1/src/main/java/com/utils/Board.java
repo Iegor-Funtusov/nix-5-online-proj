c00996de47f9dd6d672151dfd1bf80438f9d77b7
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-16 00:43:02 +0300

    Board class for displaying position added

---
+++ module_1/src/main/java/com/utils/Board.java
@@ -0,0 +1,45 @@
+package com.utils;
+
+public class Board {
+    private final int x_size = 8;
+    private final int y_size = 8;
+    public Knight knight;
+    public String[][] board = new String[8][8];
+
+    public Board() {
+        knight = new Knight(0, 0);
+        for (int i = 0; i < y_size; i++) {
+            for (int j = 0; j < x_size; j++) {
+                board[i][j] = "    ";
+            }
+        }
+        board[0][0] = knight.symbol;
+    }
+
+    public Board(int x, int y) {
+        knight = new Knight(x, y);
+        for (int i = 0; i < y_size; i++) {
+            for (int j = 0; j < x_size; j++) {
+                board[i][j] = "   ";
+            }
+        }
+        board[y][x] = knight.symbol;
+    }
+
+    public void PlacePiece(int x, int y) {
+        board[y][x] = " " + knight.symbol + " ";
+    }
+
+    public void Print() {
+        for (int i = 0; i < y_size; i++) {
+            System.out.print((knight.getY() + i - y_size) + "|");
+            for (int j = 0; j < x_size; j++) {
+                System.out.print(board[i][j] + "|");
+            }
+        }
+        for(int i = 0; i < x_size; i++) {
+            System.out.print((knight.getX() + i - x_size) + "|");
+        }
+    }
+
+}
