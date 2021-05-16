147e69f80a8b48f29ec2fffba741dee7b7592ada
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-17 00:15:05 +0300

    Run file Main.java added

---
+++ module_1/src/main/java/com/main/Main.java
@@ -0,0 +1,15 @@
+package com.main;
+
+import java.io.IOException;
+
+import static com.tasks.Controls.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+}
