d6ca4c446b43884ab802a3f0bb3cf07a91770110
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-17 00:18:52 +0300

    User CLI added and program functionality modified

--- module_1/src/main/java/com/utils/level/one/task/one/UniqueElements.java
+++ module_1/src/main/java/com/utils/level/one/task/one/UniqueElements.java
@@ -1,17 +1,62 @@
 package com.utils.level.one.task.one;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Random;
 
 public class UniqueElements {
-    public static int task1(int[] arr) {
-        ArrayList<Integer> sum = new ArrayList<>();
+
+    private static final int  r_MAX = 100;
+    private static final int  r_MIN = 0;
+
+    public static int[] InitArray(BufferedReader reader) throws IOException {
+        System.out.print("Enter the '0' if you want to input array manually and '1' if want to generate random values: ");
+        int[] array;
+        if (reader.readLine().equals("0")) {
+            System.out.print("Enter size of array: ");
+            int size = Integer.parseInt(reader.readLine());
+            array = new int[size];
+            System.out.print("Enter the array values: ");
+            String[] values = reader.readLine().split("\\s+");
+            for (int i = 0; i < size; i++) {
+                array[i] = Integer.parseInt(values[i]);
+            }
+        } else {
+            Random random = new Random();
+            int size = 10;
+            System.out.println("Array size: " + size);
+            System.out.print("Array values: ");
+            array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = random.nextInt(r_MAX - r_MIN) + r_MIN;
+                System.out.print(array[i] + " ");
+            }
+            System.out.println("");
+        }
+        return array;
+    }
+
+    public static int getMax(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int getAmountOfUnique(int[] arr) {
+        int[] values = new int[getMax(arr) + 1];
         int end_sum = 0;
         for (int j : arr) {
-            sum.set(j, 1);
+            values[j] = 1;
         }
-        for (Integer integer : sum) {
-            end_sum += integer;
+        for (int i : values) {
+            end_sum += i;
         }
         return end_sum;
     }
+
+
 }
