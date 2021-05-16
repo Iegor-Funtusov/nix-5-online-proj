fe80a5d7ec9e55ebb2e0cd005b2668f1d2497f86
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-16 11:06:09 +0300

    String Validation task added

---
+++ module_1/src/main/java/com/tasks/level/two/StringValidation.java
@@ -0,0 +1,43 @@
+package com.tasks.level.two;
+
+import java.util.Stack;
+
+public class StringValidation {
+
+    public static boolean CheckValidation(String string) {
+        Stack<Character> sequence = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i)=='(' || string.charAt(i)=='{' || string.charAt(i)=='[') {
+                sequence.push(string.charAt(i));
+            }
+            if (string.charAt(i)==')' || string.charAt(i)=='}' || string.charAt(i)==']') {
+                if (getReversedChar(string.charAt(i)) != sequence.peek()) {
+                    return false;
+                }
+                sequence.pop();
+            }
+        }
+        return sequence.isEmpty();
+    }
+
+    public static char getReversedChar(char sym) {
+        switch (sym) {
+            case '(':
+                return ')';
+            case '[':
+                return ']';
+            case '{':
+                return '}';
+            case ')':
+                return '(';
+            case ']':
+                return '[';
+            case '}':
+                return '{';
+            default:
+                System.out.println("Wrong character passed!");
+                return ' ';
+        }
+    }
+
+}
