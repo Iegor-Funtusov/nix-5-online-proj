aeedae600efb59a6862409bf41549e72f7bc6c09
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-17 00:15:59 +0300

    Piece allocation and representation edited

--- module_1/src/main/java/com/utils/level/one/task/two/KnightMove.java
+++ module_1/src/main/java/com/utils/level/one/task/two/KnightMove.java
@@ -5,6 +5,5 @@ public class KnightMove {
     public static void Move(int posx, int posy, int movex, int movey) {
         Board b = new Board(posx, posy);
         b.PlacePiece(movex, movey);
-        b.Print();
     }
 }
