3c3ac4d22fdab6eb6f4bceb686346bff391448f8
sudokun <dramviewer@yahoo.com>
2021-05-16 12:04:45 +0300

    refactor: wrap data with ArrayList

--- src/main/java/com/nixcource/level1/UniqueSymbols.java
+++ src/main/java/com/nixcource/level1/UniqueSymbols.java
@@ -1,16 +1,16 @@
 package com.nixcource.level1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UniqueSymbols {
 
-    public static int countUniqueSymbolsInArray(int[] data) {
+    public static int countUniqueSymbolsInArray(ArrayList<Integer> data) {
         HashMap<Integer, Integer> uniqueSymbols = new HashMap<>();
 
         for (int i : data) {
             uniqueSymbols.put(i, 0);
         }
-        System.out.println(uniqueSymbols);
 
         return uniqueSymbols.keySet().size();
     }
