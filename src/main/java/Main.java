c99ac529c2ee8a3fb6d9a87031c8dce6ae663628
Dima Gridin <dmitriygridin0904@gmail.com>
2021-05-16 12:43:44 +0300

    First commit. Added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserController userController = new UserController();
+        try {
+            userController.UserInterface();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
