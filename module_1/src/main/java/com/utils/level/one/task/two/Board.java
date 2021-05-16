0f6ffbbae93828d95a38a69d3386f7e72cad854d
Ivan Chukhran <ivan.chuhran@gmail.com>
2021-05-17 00:13:42 +0300

    Board state print deleted

--- module_1/src/main/java/com/utils/level/one/task/two/Board.java
+++ module_1/src/main/java/com/utils/level/one/task/two/Board.java
@@ -7,36 +7,23 @@ public class Board {
     public String[][] board = new String[8][8];
 
     public Board(int x, int y) {
-        knight = new Knight(x, y);
+        knight = new Knight(x , y);
         for (int i = 0; i < y_size; i++) {
             for (int j = 0; j < x_size; j++) {
                 board[i][j] = "   ";
             }
         }
-        board[y][x] = knight.symbol;
+        board[y % 8][x % 8] = knight.symbol;
     }
 
     public void PlacePiece(int x, int y) {
-        if (knight.isValidMove(x, y)) {
-            System.out.println("The Knight can be placed there!\n");
-            board[y][x] = " ✔ ";
+        if (knight.isValidMove(x % 8, y % 8)) {
+            System.out.println("The Knight can be placed there!");
+            board[y % 8][x % 8] = " ✔ ";
         }
         else {
-            System.out.println("The Knight can't be placed there!\n");
-            board[y][x] = " X ";
+            System.out.println("The Knight can't be placed there!");
+            board[y % 8][x % 8] = " X ";
         }
     }
-
-    public void Print() {
-        for (int i = 0; i < y_size; i++) {
-            System.out.print((knight.getY() + i - y_size) + "|");
-            for (int j = 0; j < x_size; j++) {
-                System.out.print(board[i][j] + "|");
-            }
-        }
-        for(int i = 0; i < x_size; i++) {
-            System.out.print((knight.getX() + i - x_size) + "|");
-        }
-    }
-
 }
