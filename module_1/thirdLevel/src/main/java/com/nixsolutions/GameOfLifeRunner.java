06429210683368a4ebd5ca329340e51fa57327bf
Влад Шахно <v.shahno@outlook.com>
2021-05-17 17:17:14 +0300

    commit forgotten files

---
+++ module_1/thirdLevel/src/main/java/com/nixsolutions/GameOfLifeRunner.java
@@ -0,0 +1,9 @@
+package com.nixsolutions;
+
+public class GameOfLifeRunner {
+
+    public static void PrintGame() {
+        System.out.println("In the release, everything will be and now try to...");
+
+    }
+}
