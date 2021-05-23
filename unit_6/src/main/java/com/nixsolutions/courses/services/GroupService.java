f742d0d2e6fd719d103fb8552d8da178cd2c8b4e
Veronica Velikaya <nik.velikay@gmail.com>
2021-05-23 21:03:03 +0300

    added logging

--- unit_6/src/main/java/com/nixsolutions/courses/services/GroupService.java
+++ unit_6/src/main/java/com/nixsolutions/courses/services/GroupService.java
@@ -3,6 +3,8 @@ package com.nixsolutions.courses.services;
 import com.nixsolutions.courses.data.Group;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.InstanceAlreadyExistsException;
 import java.lang.reflect.InvocationTargetException;
@@ -13,8 +15,12 @@ public class GroupService {
 
     private final static int STORAGE_CAPACITY = 10;
     private final static Group[] db = new Group[STORAGE_CAPACITY];
+    private static final Logger loggerInfo = LoggerFactory.getLogger("info");
+    private static final Logger loggerWarn = LoggerFactory.getLogger("warn");
+    private static final Logger loggerError = LoggerFactory.getLogger("error");
 
     public void create(Group group) throws InstanceAlreadyExistsException {
+        loggerInfo.info("Started creating group: " + group.getName());
         boolean created = false;
         for (int i = 0; i < db.length; i++) {
             if (db[i] == null) {
@@ -22,11 +28,15 @@ public class GroupService {
                 created = true;
                 break;
             } else if (db[i].getName().equals(group.getName())) {
+                loggerError.error("Group already exists: " + group.getName());
                 throw new InstanceAlreadyExistsException();
             }
         }
         if (!created) {
             System.out.println("Storage is full");
+            loggerError.error("Storage is full");
+        } else {
+            loggerInfo.info("Ended creating group");
         }
     }
 
@@ -35,30 +45,36 @@ public class GroupService {
             if (StringUtils.isNotBlank(name)) {
                 return getByName(name);
             } else {
+                loggerError.error("Entity does not exist");
                 throw new RuntimeException("Entity does not exist");
             }
         } catch (NullPointerException e) {
+            loggerError.error("Entity does not exist");
             throw new RuntimeException("Entity does not exist");
         }
     }
 
     public void update(Group group) {
+        loggerInfo.info("Started updating group: " + group.getName());
         Group current = read(group.getName());
         try {
             BeanUtils.copyProperties(current, group);
         } catch (IllegalAccessException | InvocationTargetException illegalAccessException) {
             illegalAccessException.printStackTrace();
         }
+        loggerInfo.info("Ended updating group");
     }
 
     public void delete(String name) {
-            Group current = read(name);
-            for (int i = 0; i < db.length; i++) {
-                if (db[i].getName().equals(current.getName())) {
-                    db[i] = null;
-                    break;
-                }
+        loggerWarn.warn("Started removing group by name: " + name);
+        Group current = read(name);
+        for (int i = 0; i < db.length; i++) {
+            if (db[i].getName().equals(current.getName())) {
+                db[i] = null;
+                break;
             }
+        }
+        loggerWarn.warn("Ended removing group");
     }
 
     public Group[] readAll() {
--- unit_6/src/main/java/com/nixsolutions/courses/services/StudentService.java
+++ unit_6/src/main/java/com/nixsolutions/courses/services/StudentService.java
@@ -4,6 +4,8 @@ import com.nixsolutions.courses.data.Group;
 import com.nixsolutions.courses.data.Student;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.lang.reflect.InvocationTargetException;
@@ -15,9 +17,13 @@ public class StudentService {
 
     private final GroupService groupService = new GroupService();
     private Student[] list;
+    private static final Logger loggerInfo = LoggerFactory.getLogger("info");
+    private static final Logger loggerWarn = LoggerFactory.getLogger("warn");
+    private static final Logger loggerError = LoggerFactory.getLogger("error");
 
 
     public void create(Student student, Group group) {
+        loggerInfo.info("Started creating student: " + student.getId());
         list = group.getList();
         student.setId(generateId(UUID.randomUUID().toString()));
         boolean created = false;
@@ -30,9 +36,12 @@ public class StudentService {
         }
         if (!created) {
             System.out.println("Storage is full");
+            loggerError.error("Storage is full");
         } else {
+            loggerInfo.info("Updating list of the group: " + group.getName());
             group.setList(list);
             groupService.update(group);
+            loggerInfo.info("Ended creating student");
         }
     }
 
@@ -42,24 +51,30 @@ public class StudentService {
                 list = group.getList();
                 return getById(id);
             } else {
+                loggerError.error("Entity does not exist");
                 throw new RuntimeException("Entity does not exist");
             }
         } catch (NullPointerException e) {
+            loggerError.error("Entity does not exist");
             throw new RuntimeException("Entity does not exist");
         }
     }
 
     public void update(Student student, Group group) {
+        loggerInfo.info("Started updating student: " + student.getId());
         list = group.getList();
         Student current = read(student.getId(), group);
         try {
             BeanUtils.copyProperties(current, student);
         } catch (IllegalAccessException | InvocationTargetException illegalAccessException) {
+            loggerError.error("Coping ");
             illegalAccessException.printStackTrace();
         }
+        loggerInfo.info("Ended updating student");
     }
 
     public void delete(String id, Group group) {
+        loggerWarn.warn("Started removing student by id: " + id);
         list = group.getList();
         Student current = read(id, group);
         for (int i = 0; i < list.length; i++) {
@@ -68,8 +83,10 @@ public class StudentService {
                 break;
             }
         }
+        loggerInfo.info("Updating group: " + group.getName());
         group.setList(list);
         groupService.update(group);
+        loggerWarn.warn("Ended removing student");
     }
 
     public Student[] readAll(Group group) {
