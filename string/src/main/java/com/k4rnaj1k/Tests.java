9335a48f6f7ba23afe3dba44c722b3a512641ca3
k4rnaj1k <karnajikplayer@gmail.com>
2021-05-29 05:07:48 +0300

    File for testing purposes with a main method

---
+++ string/src/main/java/com/k4rnaj1k/Tests.java
@@ -0,0 +1,9 @@
+package com.k4rnaj1k;
+
+public class Tests {
+    public static void main(String[] args) {
+        System.out.println(StringReverse.reverse("Hello World!"));
+        System.out.println(StringReverse.reverse("Hello World!", "World"));
+        System.out.println(StringReverse.reverse("Hello World!", 0, 4));
+    }
+}
