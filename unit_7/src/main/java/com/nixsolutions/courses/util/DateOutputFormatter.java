ce51f58b73bbf6a0108677dc55ed8990386420a2
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-08 22:10:40 +0300

    added output formatter

--- unit_7/src/main/java/com/nixsolutions/courses/util/DateOutputFormatter.java
+++ unit_7/src/main/java/com/nixsolutions/courses/util/DateOutputFormatter.java
@@ -1,4 +1,30 @@
 package com.nixsolutions.courses.util;
 
+import com.nixsolutions.courses.data.Date;
+import java.util.zip.DataFormatException;
+
 public class DateOutputFormatter {
+
+    public static String formatDate(Date date, String format) throws DataFormatException {
+        String outputDate = "";
+
+        switch (format) {
+            case "1":
+                outputDate = firstFormat(date); // dd/mm/yy hh:mm:ss
+                break;
+        }
+        return outputDate;
+    }
+
+    private static String formatTime(Date date) {
+        return String.format("%02d:%02d:%02d", date.getTime().getHours(), date.getTime().getMinutes(), date.getTime().getSeconds());
+    }
+
+    private static String firstFormat(Date date) {
+        String outputDate = String.format("%02d/%02d/%d ", date.getDay(), date.getMonth(), date.getYear());
+        outputDate += formatTime(date);
+
+        return outputDate;
+
+    }
 }
