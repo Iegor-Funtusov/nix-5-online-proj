d6b3ca832ecd61212b91c5e7e970263d0b103933
Evgeniya Lyahovec <jacky@ukr.net>
2021-06-13 17:10:59 +0300

    unit_7_exception module was added

---
+++ unit_7_exception/src/main/java/CalendarTest.java
@@ -0,0 +1,7 @@
+public class CalendarTest {
+    public static void main(String[] args) {
+        //   System.out.println("CalendarTest.main");
+        CalendarController calendarController = new CalendarController();
+        calendarController.readConsole();
+    }
+}
