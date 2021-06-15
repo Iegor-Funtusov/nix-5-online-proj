2ba0db58eeb377f5b755dd63e6d7758765675705
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-06-15 13:40:20 +0300

    Added Application class to run the app. Modified Main class

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import Controller.SetController;
+
+public class Application {
+
+    public static void run(){
+        SetController controller = new SetController();
+        controller.userInterface();
+    }
+}
