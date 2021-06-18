bf502e4b8bf3f360eac38793e48631867dd1f09b
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-18 17:46:21 +0300

    refactored DifferenceService

--- unit_7/src/main/java/com/nixsolutions/courses/service/DifferenceService.java
+++ unit_7/src/main/java/com/nixsolutions/courses/service/DifferenceService.java
@@ -7,38 +7,22 @@ import java.time.DateTimeException;
 
 public class DifferenceService {
 
-    protected double differenceInSeconds(Date from, Date to) {
-        double result = differenceInMinutes(from, to);
-        if (result != 0 || (to.getTime().getSeconds() - from.getTime().getSeconds() >= 0)) {
-            result = result * 60 + (to.getTime().getSeconds() - from.getTime().getSeconds());
-            return result;
-        } else {
-            throw new DateTimeException("Value of to date is less than of from date");
-        }
+    public double differenceInSeconds(Date from, Date to) {
+        return differenceInMinutes(from, to) * 60 + (to.getTime().getSeconds() - from.getTime().getSeconds());
     }
 
-    protected double differenceInMinutes(Date from, Date to) {
-        double result = differenceInHours(from, to);
-        if (result != 0 || (to.getTime().getMinutes() - from.getTime().getMinutes() >= 0)) {
-            result = result * 60 + (to.getTime().getMinutes() - from.getTime().getMinutes());
-            return result;
-        } else {
-            throw new DateTimeException("Value of to date is less than of from date");
-        }
+    public double differenceInMinutes(Date from, Date to) {
+        return differenceInHours(from, to) * 60 + (to.getTime().getMinutes() - from.getTime().getMinutes());
     }
 
-    protected double differenceInHours(Date from, Date to) {
-        double result = differenceInDays(from, to);
-        if (result != 0 || (to.getTime().getHours() - from.getTime().getHours() >= 0)) {
-            result = result * 24 + (to.getTime().getHours() - from.getTime().getHours());
-            return result;
-        } else {
-            throw new DateTimeException("Value of to date is less than of from date");
-        }
+    public double differenceInHours(Date from, Date to) {
+        return differenceInDays(from, to) * 24
+                + (to.getTime().getHours() - from.getTime().getHours())
+                + (double)(to.getTime().getMinutes() - from.getTime().getMinutes())/60;
     }
 
 
-    protected double differenceInDays(Date from, Date to) throws DateTimeException {
+    public double differenceInDays(Date from, Date to) throws DateTimeException {
         double result = differenceInMonths(from, to);
         if (result != 0) {
             if (result > 12) {
@@ -47,50 +31,35 @@ public class DifferenceService {
                     result += 365;
                     if (CalendarUtils.isLeapYear(i)) result++;
                 }
-                for (int i = from.getMonth(); i < 13; i++) {
-                    result += CalendarUtils.daysInMonth(i, from.getYear());
-                }
-                for (int i = to.getMonth(); i > 0; i--) {
-                    result += CalendarUtils.daysInMonth(i, from.getYear());
-                }
             } else {
                 result = 0;
                 if (from.getYear().equals(to.getYear())) {
                     for (int i = from.getMonth(); i < to.getMonth() + 1; i++) {
                         result += CalendarUtils.daysInMonth(i, from.getYear());
                     }
-                } else {
-                    for(int i = from.getMonth(); i < 13; i++) {
-                        result += CalendarUtils.daysInMonth(i, from.getYear());
-                    }
-                    for(int i = 1; i < to.getMonth() + 1; i++) {
-                        result += CalendarUtils.daysInMonth(i, to.getYear());
-                    }
+                    return result - from.getDay() - (CalendarUtils.daysInMonth(to.getMonth(), to.getYear()) - to.getDay());
                 }
             }
+            for (int i = from.getMonth(); i < 13; i++) {
+                result += CalendarUtils.daysInMonth(i, from.getYear());
+            }
+            for (int i = to.getMonth(); i > 0; i--) {
+                result += CalendarUtils.daysInMonth(i, to.getYear());
+            }
             result = result - from.getDay() - (CalendarUtils.daysInMonth(to.getMonth(), to.getYear()) - to.getDay());
-
         }
         return result;
     }
 
-    protected double differenceInMonths(Date from, Date to) {
-        double result = differenceInYears(from, to);
-        if (result != 0 || (to.getMonth() - from.getMonth() >= 0)) {
-            result = result * 12 + (double) (to.getDay() - from.getDay()) / 31;
-            return result;
-        } else {
-            throw new DateTimeException("Value of to date is less than of from date");
-        }
+    public double differenceInMonths(Date from, Date to) {
+        return differenceInYears(from, to) * 12
+                + (double) (to.getDay() - from.getDay()) / 31;
     }
 
-    protected double differenceInYears(Date from, Date to) {
-        double result = to.getYear() - from.getYear();
-        if (result >= 0) {
-            result = result + (double) (to.getMonth() - from.getMonth()) / 12;
-            return result;
-        } else {
-            throw new DateTimeException("Value of to date is less than of from date");
-        }
+    public double differenceInYears(Date from, Date to) throws DateTimeException {
+        if (CalendarUtils.validDatePair(from, to))
+            return to.getYear() - from.getYear()
+                    + (double) (to.getMonth() - from.getMonth()) / 12;
+        throw new DateTimeException("Value of to date is less than of from date");
     }
 }
