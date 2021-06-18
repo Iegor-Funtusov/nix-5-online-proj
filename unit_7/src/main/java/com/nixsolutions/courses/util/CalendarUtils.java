757d57bf2eb4d27c9f40a30ab8d98b4679202d6f
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-18 17:44:57 +0300

    added validDatePair method in utils

--- unit_7/src/main/java/com/nixsolutions/courses/util/CalendarUtils.java
+++ unit_7/src/main/java/com/nixsolutions/courses/util/CalendarUtils.java
@@ -1,6 +1,50 @@
 package com.nixsolutions.courses.util;
 
+import com.nixsolutions.courses.data.Date;
+import com.nixsolutions.courses.data.Time;
+
 public class CalendarUtils {
+
+    public static boolean validDatePair(Date from, Date to) {
+        boolean result = false;
+        if (from.getYear() <= to.getYear()) {
+            result = true;
+            if (from.getYear().equals(to.getYear())) {
+                if (from.getMonth() <= to.getMonth()) {
+                    if (from.getMonth().equals(to.getMonth())) {
+                        if (from.getDay() <= to.getDay()) {
+                            if (from.getDay().equals(to.getDay())) {
+                                return validTimePair(from.getTime(), to.getTime());
+                            }
+                        } else {
+                            result = false;
+                        }
+                    }
+                } else {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean validTimePair(Time from, Time to) {
+        boolean result = false;
+        if (from.getHours() <= to.getHours()) {
+            result = true;
+            if (from.getHours().equals(to.getHours())) {
+                if (from.getMinutes() <= to.getMinutes()) {
+                    if (from.getMinutes().equals(to.getMinutes())) {
+                        if (!(from.getSeconds() <= to.getSeconds()))
+                            result = false;
+                    }
+                } else {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
 
     public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
