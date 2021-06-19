f3f1b0d9b846a1b1adb8eeac0f586bff1f084bd9
Sheblyk <kokosigel@gmail.com>
2021-06-19 22:03:07 +0300

    entities and services refactor

--- exceptions/src/main/java/ua/com/exceptions/entity/Calendar.java
+++ exceptions/src/main/java/ua/com/exceptions/entity/Calendar.java
@@ -1,35 +1,122 @@
 package ua.com.exceptions.entity;
 
-public class Calendar {
-    private Date date;
-    private Time time;
+import java.util.Objects;
 
-    public Calendar(Date date, Time time) {
-        this.date = date;
-        this.time = time;
+public class Calendar implements Comparable <Calendar> {
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Calendar(int year, int month, int day, int hour, int minute, int second) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
     }
 
-    public Date getDate() {
-        return date;
+    public int getYear() {
+        return year;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setYear(int year) {
+        this.year = year;
     }
 
-    public Time getTime() {
-        return time;
+    public int getMonth() {
+        return month;
     }
 
-    public void setTime(Time time) {
-        this.time = time;
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Calendar calendar = (Calendar) o;
+        return year == calendar.year &&
+                month == calendar.month &&
+                day == calendar.day &&
+                hour == calendar.hour &&
+                minute == calendar.minute &&
+                second == calendar.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, month, day, hour, minute, second);
     }
 
     @Override
     public String toString() {
         return "Calendar{" +
-                "date=" + date +
-                ", time=" + time +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                ", hour=" + hour +
+                ", minute=" + minute +
+                ", second=" + second +
                 '}';
     }
+
+    @Override
+    public int compareTo(Calendar c) {
+        int comp = Integer.compare(year, c.year);
+        if(comp == 0){
+            comp = Integer.compare(month, c.month);
+            if(comp == 0){
+                comp = Integer.compare(day, c.day);
+                if(comp == 0){
+                    comp = Integer.compare(hour, c.hour);
+                    if(comp == 0){
+                        comp = Integer.compare(minute, c.minute);{
+                            if(comp == 0){
+                                comp = Integer.compare(second, c.second);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return comp;
+    }
 }
+
--- exceptions/src/main/java/ua/com/exceptions/services/CalendarService.java
+++ exceptions/src/main/java/ua/com/exceptions/services/CalendarService.java
@@ -1,18 +1,101 @@
 package ua.com.exceptions.services;
 
 import ua.com.exceptions.entity.Calendar;
-import ua.com.exceptions.entity.Date;
-import ua.com.exceptions.entity.Time;
 
 public class CalendarService {
-    private static DateService dateService = new DateService();
-    private static TimeService timeService = new TimeService();
 
-    public static Calendar checkCalendar(int year, int month, int day, int hours, int minutes, int seconds){
-        if(dateService.checkDate(year, month, day) != null &&
-                timeService.checkTime(hours, minutes, seconds)!= null){
-            return new Calendar(new Date(year, month, day), new Time(hours, minutes, seconds));
+    public Calendar checkDate(int year, int month, int day, int hours, int minutes, int seconds){
+        try{
+            if(validYear(year) &&
+                    validMonth(month) &&
+                    validDay(day, month, year) &&
+                    validHour(hours) &&
+                    validMinute(minutes)
+                    && validSeconds(seconds)
+            ){
+                return new Calendar(year, month, day, hours, minutes, seconds);
+            }
+        }
+        catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
         }
         return null;
     }
+
+    private boolean validYear(int year){
+        if(year > 0 && year <=10000){
+            return true;
+        }
+        else{
+            throw new RuntimeException("Error! Year should be in a range 1-10_000");
+        }
+    }
+
+    private boolean validMonth(int month){
+        if(month > 0 && month <= 12){
+            return true;
+        }
+        else{
+            throw new RuntimeException("Error! Month should be in a range 1-12");
+        }
+    }
+
+    private boolean validDay(int day, int month, int year){
+        if(day > 0 && day <= getMonthCount(month, year)){
+            return true;
+        }
+        else {
+            throw new RuntimeException("Error! Day should be in a range 1-(28/29/30/31) depends on month!");
+        }
+    }
+
+    public static int getMonthCount(int month, int year){
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+                || month == 10 || month == 12) {
+            return 31;
+        }
+        if(month == 2) {
+            return 28 + leapYearCheck(year);
+        }
+        else {
+            return 30;
+        }
+    }
+
+    public static int leapYearCheck(int year){
+        if(year%4 == 0 || (year%100 == 0 && year%400 == 0)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    private boolean validHour(int hours){
+        if(hours >=0 && hours <= 23){
+            return true;
+        }
+        else{
+            throw new RuntimeException("Error! Hour should be in a range of 0-23");
+        }
+    }
+
+    private boolean validMinute(int minutes){
+        if(minutes >=0 && minutes <= 59){
+            return true;
+        }
+        else {
+            throw new RuntimeException("Error! Minutes should be in a range of 0-59");
+        }
+    }
+
+    private boolean validSeconds(int seconds){
+        if(seconds >=0 && seconds <= 59){
+            return true;
+        }
+        else {
+            throw new RuntimeException("Error! Seconds should be in a range of 0-59");
+        }
+    }
+
 }
