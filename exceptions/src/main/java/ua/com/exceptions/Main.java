8347dd3a7644c7af4c5330b512d041232c0e00cd
Sheblyk <kokosigel@gmail.com>
2021-06-21 03:15:59 +0300

    added Main and changed pom.xml: added shade plugin

---
+++ exceptions/src/main/java/ua/com/exceptions/Main.java
@@ -0,0 +1,10 @@
+package ua.com.exceptions;
+
+import ua.com.exceptions.services.GeneralService;
+
+public class Main {
+    public static void main(String[] args) {
+        GeneralService generalService = new GeneralService();
+        generalService.run();
+    }
+}
