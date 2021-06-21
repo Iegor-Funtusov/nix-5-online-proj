5836ca5dc8dee4a1cb4d5229289b50f93badb72c
Sheblyk <kokosigel@gmail.com>
2021-06-21 15:47:43 +0300

    init commit

---
+++ collections/src/main/java/ua/com/collections/MathSet.java
@@ -0,0 +1,85 @@
+package ua.com.collections;
+
+public class MathSet<N extends Number & Comparable<N>>
+        implements MathSetInterface<N>{
+
+    public void add(N n) {
+
+    }
+
+    public void add(N... n) {
+
+    }
+
+    public void join(MathSet ms) {
+
+    }
+
+    public void join(MathSet... ms) {
+
+    }
+
+    public void sortDesc() {
+
+    }
+
+    public void sortDesc(int firstIndex, int lastIndex) {
+
+    }
+
+    public void sortDesc(N value) {
+
+    }
+
+    public void sortAsc() {
+
+    }
+
+    public void sortAsc(int firstIndex, int lastIndex) {
+
+    }
+
+    public void sortAsc(Number value) {
+
+    }
+
+    public N get(int index) {
+        return null;
+    }
+
+    public N getMax() {
+        return null;
+    }
+
+    public N getMin() {
+        return null;
+    }
+
+    public N getAverage() {
+        return null;
+    }
+
+    public N getMedian() {
+        return null;
+    }
+
+    public N[] toArray() {
+        return null;
+    }
+
+    public N[] toArray(int firstIndex, int lastIndex) {
+        return null;
+    }
+
+    public MathSet squash(int firstIndex, int lastIndex) {
+        return null;
+    }
+
+    public void clear() {
+
+    }
+
+    public void clear(Number[] numbers) {
+
+    }
+}
