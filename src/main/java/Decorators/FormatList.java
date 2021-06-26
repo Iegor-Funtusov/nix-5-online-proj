a695c8351b7b67676b2c3bd6775548b6b5086e5d
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-06-27 00:44:38 +0300

    Small fixes

--- src/main/java/Decorators/FormatList.java
+++ src/main/java/Decorators/FormatList.java
@@ -7,7 +7,7 @@ public class FormatList{
 
     public static List<? extends BaseEntity> formatToOutput(List<? extends BaseEntity> listToFormat){
        if(listToFormat.isEmpty() || listToFormat.size() == 1){
-           throw new RuntimeException("There are no books");
+           throw new RuntimeException("There are no elements");
        }
 
        listToFormat.remove(listToFormat.get(0));
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main.main");
+    public static void main(String[] args){
+        Application.run();
     }
 }
