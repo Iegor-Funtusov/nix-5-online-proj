ff2ac5ec39e673b0bf079afdbeaee4581f4b9b6b
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-27 17:36:41 +0300

    added AppMain class

--- unit_9/src/main/java/com/nixsolutions/courses/AppMain.java
+++ unit_9/src/main/java/com/nixsolutions/courses/AppMain.java
@@ -1,4 +1,11 @@
 package com.nixsolutions.courses;
 
+import com.nixsolutions.courses.controller.ConsoleController;
+
 public class AppMain {
+
+    public static void main(String[] args) {
+        ConsoleController consolecontroller = new ConsoleController();
+        consolecontroller.printMenu();
+    }
 }
