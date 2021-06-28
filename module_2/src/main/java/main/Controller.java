b4a8d18c51e23716da2834d7279eb7303d0b187e
Unusual-Intellectual <leshalitova@gmail.com>
2021-06-28 18:43:35 +0300

    UI and first version of solutions

--- module_2/src/main/java/main/Controller.java
+++ module_2/src/main/java/main/Controller.java
@@ -19,23 +19,33 @@ import java.util.Scanner;
 public class Controller {
     public void start(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input action:\n" +
-                "1 >> convert dates\n" +
-                "2 >> find first unique name\n" +
-                "3 >> find minimum distances between cities");
-        String choice = sc.nextLine();
-        switch (choice){
-            case "1":{
-                converterOfDates();
-            }break;
-            case "2":{
-                findUniqueName();
-            }break;
-            case "3":{
-                cities();
-            }break;
-            default:
-                System.out.println("Wrong input");
+        while (true){
+            System.out.println("Input action:\n" +
+                    "1 >> convert dates\n" +
+                    "2 >> find first unique name\n" +
+                    "3 >> find minimum distances between cities\n" +
+                    "4 >> exit from program");
+            String choice = sc.nextLine();
+            switch (choice) {
+                case "1": {
+                    converterOfDates();
+                }
+                break;
+                case "2": {
+                    findUniqueName();
+                }
+                break;
+                case "3": {
+                    cities();
+                }
+                break;
+                case "4": {
+                    System.exit(0);
+                }
+                break;
+                default:
+                    System.out.println("Wrong input");
+            }
         }
     }
 
