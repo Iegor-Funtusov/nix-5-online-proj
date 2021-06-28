38a09fe6d4f02059d6d209660698a35b4d89e89f
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-28 16:46:04 +0300

    added methods to CSVParser

--- unit_9/src/main/java/com/nixsolutions/courses/util/CSVParser.java
+++ unit_9/src/main/java/com/nixsolutions/courses/util/CSVParser.java
@@ -17,10 +17,10 @@ public class CSVParser {
     private static final String LIST_REGEX = ",";
 
     public static Book readBook(String id) throws NoSuchElementException {
-        try(CSVReader reader = new CSVReader(new FileReader(FilePaths.BOOKS.getPath()))) {
+        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.BOOKS.getPath()))) {
             List<String[]> books = reader.readAll();
             for (String[] book : books) {
-                if (book[0].equals(id)) return parseBook(book);
+                if (book[0].equals(id) && Boolean.parseBoolean(book[3])) return parseBook(book);
             }
         } catch (IOException | CsvException e) {
             e.printStackTrace();
@@ -28,16 +28,36 @@ public class CSVParser {
         throw new NoSuchElementException("No element with such id found");
     }
 
+    public static List<String[]> readAllBooks() {
+        List<String[]> data = null;
+        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.BOOKS.getPath()))) {
+            data = reader.readAll();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public static Author readAuthor(String id) {
-        try(CSVReader reader = new CSVReader(new FileReader(FilePaths.AUTHORS.getPath()))) {
+        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.AUTHORS.getPath()))) {
             List<String[]> authors = reader.readAll();
             for (String[] author : authors) {
-                if (author[0].equals(id)) return parseAuthor(author);
+                if (author[0].equals(id) && Boolean.parseBoolean(author[4])) return parseAuthor(author);
             }
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
         throw new NoSuchElementException("No element with such id found");
+    }
+
+    public static List<String[]> readAllAuthors() {
+        List<String[]> data = null;
+        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.AUTHORS.getPath()))) {
+            data = reader.readAll();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
     private static Author parseAuthor(String[] data) throws IOException {
@@ -45,25 +65,41 @@ public class CSVParser {
         author.setId(data[0]);
         author.setName(data[1]);
         author.setSurname(data[2]);
-        author.setBooks(parseIds(data[3]));
+        author.setBooks(parseIdsList(data[3]));
         author.setVisible(Boolean.parseBoolean(data[4]));
         return author;
+    }
+
+    public static List<Author> parseAllAuthors() throws IOException {
+        List<String[]> data = readAllAuthors();
+        List<Author> authors = new ArrayList<>();
+        for (String[] item : data) {
+//            if (Boolean.parseBoolean(item[4]))
+            authors.add(parseAuthor(item));
+        }
+        return authors;
     }
 
     private static Book parseBook(String[] data) throws IOException {
         Book book = new Book();
         book.setId(data[0]);
         book.setTitle(data[1]);
-        book.setAuthors(parseIds(data[2]));
+        book.setAuthors(parseIdsList(data[2]));
         book.setVisible(Boolean.parseBoolean(data[3]));
         return book;
     }
 
-    public static List<String> parseIds(String data) throws IOException {
-        if (!data.isEmpty()) {
-            String[] ids = data.split(LIST_REGEX);
-            return new ArrayList<>(Arrays.asList(ids));
+    public static List<Book> parseAllBooks() throws IOException {
+        List<String[]> data = readAllBooks();
+        List<Book> books = new ArrayList<>();
+        for (String[] item : data) {
+                books.add(parseBook(item));
         }
-        throw new IOException("Empty string");
+        return books;
+    }
+
+    public static List<String> parseIdsList(String data) {
+        String[] ids = data.split(LIST_REGEX);
+        return new ArrayList<>(Arrays.asList(ids));
     }
 }
