8e6e409e0796c2bd3fc0d8d9180a6c167857df3f
Veronica Velikaya <nik.velikay@gmail.com>
2021-06-28 21:13:59 +0300

    updated create methods

--- unit_9/src/main/java/com/nixsolutions/courses/service/LibraryService.java
+++ unit_9/src/main/java/com/nixsolutions/courses/service/LibraryService.java
@@ -4,15 +4,10 @@ import com.nixsolutions.courses.data.Author;
 import com.nixsolutions.courses.data.Book;
 import com.nixsolutions.courses.service.impl.AuthorService;
 import com.nixsolutions.courses.service.impl.BookService;
-import com.nixsolutions.courses.util.FilePaths;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+import com.nixsolutions.courses.util.CSVParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class LibraryService {
@@ -34,7 +29,7 @@ public class LibraryService {
     }
 
     public void createBook(Book book) {
-        List<String> authors = checkAuthors(book.getAuthors());
+        List<String> authors = checkAuthors(book);
         if (authors.size() == 0) {
             loggerError.error("No valid authors of book:" + "(id=" + book.getId() + ",title=" + book.getTitle() + ")");
             throw new RuntimeException("No valid authors of book" + "(id=" + book.getId() + ",title=" + book.getTitle() + ")");
@@ -43,8 +38,30 @@ public class LibraryService {
         bookService.create(book);
     }
 
+    private List<String> checkAuthors(Book book) {
+        loggerInfo.info("Checking authors of book: " + "(id=" + book.getId() + ",title=" + book.getTitle() + ")");
+        List<String> data = book.getAuthors();
+        List<String[]> authors = CSVParser.readAllAuthors();
+        List<String> authorsIds = CSVParser.getIds(authors);
+        for (String id : data) {
+            if (!authorsIds.contains(id)) {
+                loggerWarn.warn("Author does not exist: " + "id=" + id);
+                data.remove(id);
+            } else {
+                Author author = authorService.findById(id);
+                List<String> bookList = author.getBooks();
+                if (!bookList.contains(book.getId())) {
+                    bookList.add(book.getId());
+                    author.setBooks(bookList);
+                    authorService.update(author);
+                }
+            }
+        }
+        return data;
+    }
+
     public void createAuthor(Author author) {
-        List<String> books = checkBooks(author.getBooks());
+        List<String> books = checkBooks(author);
         if (books.size() == 0) {
             loggerWarn.warn("No valid books of author:" + "(id=" + author.getId() + ",name=" + author.getName() + ",surname=" + author.getSurname() + ")");
         }
@@ -52,40 +69,27 @@ public class LibraryService {
         authorService.create(author);
     }
 
-    private List<String> checkAuthors(List<String> data) {
-        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.AUTHORS.getPath()))) {
-            List<String[]> authors = reader.readAll();
-            List<String> authorsIds = new ArrayList<>();
-            for (String[] author : authors) {
-                authorsIds.add(author[0]);
-            }
-            for (String id : data) {
-                if (!authorsIds.contains(id)) {
-                    loggerWarn.warn("Author does not exist: " + "id=" + id);
-                    data.remove(id);
-                }
-            }
-        } catch (IOException | CsvException e) {
-            e.printStackTrace();
+    private List<String> checkBooks(Author author) {
+        loggerInfo.info("Checking books of author:" + "(id=" + author.getId() + ",name=" + author.getName() + ",surname=" + author.getSurname() + ")");
+        List<String> data = author.getBooks();
+        List<String[]> books = CSVParser.readAllBooks();
+        List<String> booksIds = CSVParser.getIds(books);
+        for (String[] book : books) {
+            booksIds.add(book[0]);
         }
-        return data;
-    }
-
-    private List<String> checkBooks(List<String> data) {
-        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.BOOKS.getPath()))) {
-            List<String[]> books = reader.readAll();
-            List<String> booksIds = new ArrayList<>();
-            for (String[] book : books) {
-                booksIds.add(book[0]);
-            }
-            for (String id : data) {
-                if (!booksIds.contains(id)) {
-                    loggerWarn.warn("Book does not exist: " + "id=" + id);
-                    data.remove(id);
+        for (String id : data) {
+            if (!booksIds.contains(id)) {
+                loggerWarn.warn("Book does not exist: " + "id=" + id);
+                data.remove(id);
+            } else {
+                Book book = bookService.findById(id);
+                List<String> authorList = book.getAuthors();
+                if (!authorList.contains(book.getId())) {
+                    authorList.add(book.getId());
+                    author.setBooks(authorList);
+                    bookService.update(book);
                 }
             }
-        } catch (IOException | CsvException e) {
-            e.printStackTrace();
         }
         return data;
     }
@@ -113,32 +117,6 @@ public class LibraryService {
     public List<Author> readAllAuthors() {
         return authorService.readAll();
     }
-
-//    private boolean alreadyExist(Book book) throws FileNotFoundException {
-//        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.BOOKS.getPath()))) {
-//            List<String[]> books = reader.readAll();
-//            for (String[] fields : books) {
-//                if (fields[1].equals(book.getTitle()) && CSVParser.parseIds(fields[2]).equals(book.getAuthors()))
-//                    return true;
-//            }
-//        } catch (IOException | CsvException e) {
-//            e.printStackTrace();
-//        }
-//        return false;
-//    }
-
-//    private boolean alreadyExist(Author author) {
-//        try (CSVReader reader = new CSVReader(new FileReader(FilePaths.AUTHORS.getPath()))) {
-//            List<String[]> authors = reader.readAll();
-//            for (String[] fields : authors) {
-//                if (fields[1].equals(author.getName()) && fields[2].equals(author.getSurname()) && CSVParser.parseIds(fields[3]).equals(author.getBooks()))
-//                    return true;
-//            }
-//        } catch (IOException | CsvException e) {
-//            e.printStackTrace();
-//        }
-//        return false;
-//    }
 
     public Book getBookById(String id) {
         return bookService.findById(id);
