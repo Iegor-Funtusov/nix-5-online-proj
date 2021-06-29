47a06fbf2e9aa4a4cae7885339ebaaf1e000cce2
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-06-29 17:51:54 +0300

    add first task

--- module2/src/main/java/ua/com/nkrasnovoronka/Main.java
+++ module2/src/main/java/ua/com/nkrasnovoronka/Main.java
@@ -1,6 +1,10 @@
 package ua.com.nkrasnovoronka;
 
+import ua.com.nkrasnovoronka.task1.DateFormatCheck;
+
 public class Main {
     public static void main(String[] args) {
+        DateFormatCheck dateFormatCheck = new DateFormatCheck();
+        dateFormatCheck.writeValidDatesToFile("src/main/resources/task2/input.txt", "src/main/resources/task2/output.txt");
     }
 }
--- module2/src/main/java/ua/com/nkrasnovoronka/task1/DateFormatCheck.java
+++ module2/src/main/java/ua/com/nkrasnovoronka/task1/DateFormatCheck.java
@@ -1,4 +1,49 @@
 package ua.com.nkrasnovoronka.task1;
 
+import ua.com.nkrasnovoronka.util.FileUtil;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class DateFormatCheck {
+
+    private static final String DATE_PATTERN_1 = "^\\d{4}\\/\\d{2}\\/\\d{2}$";
+    private static final String DATE_PATTERN_2 = "^\\d{2}\\/\\d{2}\\/\\d{4}$";
+    private static final String DATE_PATTERN_3 = "^\\d{2}-\\d{2}-\\d{4}$";
+
+    private static final String OUTPUT_FORMAT = "yyyyMMdd";
+
+    private static final String DATE_FORMAT_1 = "yyyy/MM/dd";
+    private static final String DATE_FORMAT_2 = "dd/MM/yyyy";
+    private static final String DATE_FORMAT_3 = "MM-dd-yyyy";
+
+    public void writeValidDatesToFile(String inputFile, String outputFile) {
+        List<String> collect = FileUtil.readFile(inputFile).stream()
+                .map(this::formatDate)
+                .collect(Collectors.toList());
+
+        FileUtil.writeToFile(collect, outputFile);
+    }
+
+    private String formatDate(String str) {
+        Map<Pattern, DateTimeFormatter> dateFormats = Map.of(
+                Pattern.compile(DATE_PATTERN_1), DateTimeFormatter.ofPattern(DATE_FORMAT_1),
+                Pattern.compile(DATE_PATTERN_2), DateTimeFormatter.ofPattern(DATE_FORMAT_2),
+                Pattern.compile(DATE_PATTERN_3), DateTimeFormatter.ofPattern(DATE_FORMAT_3)
+        );
+
+        return dateFormats.keySet().stream()
+                .filter(pattern -> pattern.matcher(str).matches())
+                .map(dateFormats::get)
+                .map(dtf -> dtf.parse(str))
+                .map(df -> DateTimeFormatter.ofPattern(OUTPUT_FORMAT).format(df))
+                .findFirst()
+                .orElse(System.lineSeparator());
+
+    }
+
 }
