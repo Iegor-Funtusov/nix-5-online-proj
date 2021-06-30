a78b914a8d606ed5ba462db32d920800bc7c0552
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-06-30 10:14:49 +0300

    add UniqueNameFinder task

--- module2/src/main/java/ua/com/nkrasnovoronka/task2/UniqueNameFinder.java
+++ module2/src/main/java/ua/com/nkrasnovoronka/task2/UniqueNameFinder.java
@@ -1,4 +1,25 @@
 package ua.com.nkrasnovoronka.task2;
 
+import ua.com.nkrasnovoronka.util.FileUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class UniqueNameFinder {
+    public void getFirstUniqueName(String filePath, String outputFile){
+        List<String> namesFromFile = FileUtil.readFile(filePath);
+        Map<String, Long> frequency = namesFromFile.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Map.Entry<String, Long> e : frequency.entrySet()){
+            if(e.getValue() == 1){
+                FileUtil.writeToFile(Collections.singletonList(e.getKey()), outputFile);
+                break;
+            }else {
+                FileUtil.writeToFile(Collections.singletonList("Current file doesn't contains unique names"), outputFile);
+            }
+        }
+    }
 }
