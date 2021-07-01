7992b9b53d682e55a2a7599b4c3c599f04f529b9
Sheblyk <kokosigel@gmail.com>
2021-07-01 23:28:26 +0300

    finished third task

---
+++ module_2/src/main/java/task3/InfoFromToFile.java
@@ -0,0 +1,55 @@
+package task3;
+
+import java.io.*;
+
+public class InfoFromToFile {
+    int [] result;
+
+    public void getInfoFromFile(String path) {
+        try (BufferedReader scanner = new BufferedReader(new FileReader(path))) {
+        int citiesCount = Integer.parseInt(scanner.readLine());
+        int [][] graph = new int [citiesCount][citiesCount];
+        String [] citiesNames = new String[citiesCount];
+        for(int i = 0; i < citiesCount; i++){
+            String city = scanner.readLine();
+            citiesNames[i] = city;
+            int countNodes = Integer.parseInt(scanner.readLine());
+            for(int j = 0; j < countNodes; j++) {
+                String s = scanner.readLine();
+                String[] divider = s.split("\\s+");
+                graph[i][Integer.parseInt(divider[0]) - 1] = Integer.parseInt(divider[1]);
+            }
+        }
+        ShortestPath shortestPath = new ShortestPath();
+        shortestPath.setCities(citiesCount);
+        shortestPath.setGraph(graph);
+        shortestPath.setCitiesNames(citiesNames);
+        int searchPathCount = Integer.parseInt(scanner.readLine());
+        if(searchPathCount <=100) {
+            result = new int[searchPathCount];
+        }
+        else {
+            throw new IllegalArgumentException("Paths` count should be less tnan 100");
+        }
+        for(int k = 0; k < result.length; k++){
+            String task = scanner.readLine();
+            String[] divider = task.split("\\s+");
+            result[k] = shortestPath.dijkstra(shortestPath.getCityIndex(divider[0]),
+                    shortestPath.getCityIndex(divider[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setInfoToFile(String path){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(path, false))){
+            for (int value : result) {
+                writer.write(value + "\n");
+            }
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ module_2/src/main/java/task3/ShortestPath.java
@@ -0,0 +1,63 @@
+package task3;
+public class ShortestPath {
+        int cities;
+        int [][] graph;
+        String[] citiesNames;
+
+    public int getCityIndex(String s){
+        for(int i = 0; i < citiesNames.length; i++) {
+            if (citiesNames[i].equals(s)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("City doesn`t exist");
+    }
+
+    public void setCities(int cities) {
+        if(cities <= 10000){
+            this.cities = cities;
+        }
+        else {
+            throw new IllegalArgumentException("Cities` count should be less than 10000");
+        }
+    }
+
+    public void setGraph(int[][] graph) {
+        this.graph = graph;
+    }
+
+    public void setCitiesNames(String[] citiesnames) {
+        this.citiesNames = citiesnames;
+    }
+
+    int minDistance(int[] path_array, boolean[] sptSet)   {
+            int min = Integer.MAX_VALUE,
+                minV = -1;
+            for (int v = 0; v < cities; v++)
+                if (!sptSet[v] && path_array[v] <= min) {
+                    min = path_array[v];
+                    minV = v;
+                }
+            return minV;
+        }
+
+        public int dijkstra(int src_node, int end_node)  {
+            int[] paths = new int[cities];
+            boolean[] visited = new boolean[cities];
+            for (int i = 0; i < cities; i++) {
+                paths[i] = Integer.MAX_VALUE;
+                visited[i] = false;
+            }
+            paths[src_node] = 0;
+            for (int count = 0; count < cities - 1; count++) {
+                int u = minDistance(paths, visited);
+                visited[u] = true;
+                for (int v = 0; v < cities; v++)
+                    if (!visited[v] && graph[u][v] != 0
+                            && paths[u] != Integer.MAX_VALUE
+                            && paths[u] + graph[u][v] < paths[v])
+                        paths[v] = paths[u] + graph[u][v];
+            }
+            return paths[end_node];
+        }
+    }
